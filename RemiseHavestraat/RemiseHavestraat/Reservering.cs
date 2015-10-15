8c833d494d9273f5fb2090d90fa4d791c6cf5e13
LaurentLardenois <laurentlardenois@hotmail.nl>
2015-10-15 14:12:13 +0200

    Beetje spul gemaakt

---
+++ RemiseHavestraat/RemiseHavestraat/Reservering.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemiseHavestraat
+{
+    class Reservering
+    {
+        public int ID { get; private set; }
+        public Tram tram { get; private set; }
+        public Spoor spoor { get; private set; }
+
+        public Reservering(int id, Tram tram, Spoor spoor)
+        {
+            this.ID = id;
+            this.tram = tram;
+            this.spoor = spoor;
+        }
+    }
+}
