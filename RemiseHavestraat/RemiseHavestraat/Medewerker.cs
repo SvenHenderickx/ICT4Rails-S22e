f601f7f6b32776a5f3ae760ebe0ca9217cd1fe15
TomR1997 <tom12697@gmail.com>
2015-10-15 13:28:56 +0200

    Medewerker en tram klasse toegevoegd

---
+++ RemiseHavestraat/RemiseHavestraat/Medewerker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemiseHavestraat
+{
+    public class Medewerker
+    {
+        public string MedewerkerNaam { get; private set; }
+        bool MedewerkerBeschikbaar = false;
+
+        public Medewerker(string medewerkerNaam)
+        {
+            this.MedewerkerNaam = medewerkerNaam;
+        }
+    }
+}
---
+++ RemiseHavestraat/RemiseHavestraat/Tram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemiseHavestraat
+{
+    public class Tram
+    {
+        public enum Type { Combino, Dubbelkop, Opleidingstrams, ElfG, TwaalfG };
+        public enum Status { Defect, Schoonmaak, Dienst, Remise };
+
+        public int Lengte { get; private set; }
+        public int TramNummer { get; private set; }
+        public Type TreinType { get; private set; }
+        public Status TreinStatus { get; private set; }
+
+        public Tram(int tramNummer, int lengte, int type, int status)
+        {
+            this.TramNummer = tramNummer;
+            this.Lengte = lengte;
+            this.TreinType = (Type)type;
+            this.TreinStatus = (Status)status;
+            // enums initialiseren       
+        }
+    }
+}
