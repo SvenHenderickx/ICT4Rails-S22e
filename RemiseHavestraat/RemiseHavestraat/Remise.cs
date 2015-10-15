0f0ed7760788a1173dede4e69205273441585cb2
LaurentLardenois <laurentlardenois@hotmail.nl>
2015-10-15 10:39:28 +0200

    Beginsel

---
+++ RemiseHavestraat/RemiseHavestraat/Remise.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemiseHavestraat
+{
+    class Remise
+    {
+        #region RemiseSpecs
+        private static Remise instance;
+
+        private Remise() {}
+
+        public static Remise Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Remise();
+                }
+                return instance;
+            }
+        }
+
+        public string Naam
+        {
+            get;
+            private set;
+        }
+
+        public bool RemiseNaam(string naam)
+        {
+            Naam = naam;
+            return true;
+        }
+        #endregion
+
+        #region Initialisation
+        public bool InlogControle(string inlognaam, string wachtwoord)
+        {
+            return false;
+        }
+        #endregion
+
+
+    }
+}
