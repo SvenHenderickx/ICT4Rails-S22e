8c29670b3abf0525fa785402323e2b2c41813742
LaurentLardenois <laurentlardenois@hotmail.nl>
2015-10-15 14:36:50 +0200

    Spoor + Segment

--- RemiseHavestraat/RemiseHavestraat/Segment.cs
+++ RemiseHavestraat/RemiseHavestraat/Segment.cs
@@ -7,5 +7,15 @@ namespace RemiseHavestraat
 {
     public class Segment
     {
+        public int SegmentID { get; private set; }
+        public int SegmentNummer { get; private set; }
+        public int Lengte { get; private set; }
+
+        public Segment(int segmentID, int segmentNummer, int lengte)
+        {
+            this.SegmentID = segmentID;
+            this.SegmentNummer = segmentNummer;
+            this.Lengte = lengte;
+        }
     }
 }
--- RemiseHavestraat/RemiseHavestraat/Spoor.cs
+++ RemiseHavestraat/RemiseHavestraat/Spoor.cs
@@ -7,5 +7,13 @@ namespace RemiseHavestraat
 {
     public class Spoor
     {
+        public int SpoorNummer { get; private set; }
+        public List<Segment> Segmenten { get; private set; }
+
+        public Spoor(int spoorNummer, List<Segment> segmenten)
+        {
+            this.SpoorNummer = spoorNummer;
+            this.Segmenten = segmenten;
+        }
     }
 }
