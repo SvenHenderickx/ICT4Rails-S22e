61a3aec0b1decd99f2070e2f2c7bb0b6a772e9ab
Laurent Lardenois <laurentlardenois@hotmail.nl>
2015-11-05 12:16:47 +0100

    1 zin
    1 zin

--- RemiseHavestraat/RemiseHavestraat/Klassen/Database.cs
+++ RemiseHavestraat/RemiseHavestraat/Klassen/Database.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RemiseHavestraat
 {
     public class Database
     {
         #region Fields
+        string connString = "host= serverName;database=myDatabase;uid=userName;pwd=passWord";
+
         #endregion
 
         #region Initialisatie
